95345a6b3967320aa7923d4ca75ed385a9389c7b
David Horres <david.c.horres@jpl.nasa.gov>
2016-01-25 15:14:14 -0800

    Get a basic traversal to run

--- neuron/src/main/java/gov/nasa/jpl/audrey/cortex/neuron/Neuron.java
+++ neuron/src/main/java/gov/nasa/jpl/audrey/cortex/neuron/Neuron.java
@@ -1,17 +1,31 @@
 package gov.nasa.jpl.audrey.cortex.neuron;
 
+import java.util.ArrayList;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 import com.thinkaurelius.titan.core.TitanGraph;
 import com.thinkaurelius.titan.core.TitanFactory;
+
 
 public class Neuron {
 
     public static void main(String[] args) {
         try {
-            TitanGraph graph = TitanFactory.build()
-                    .set("storage.backend", "cassandra")
-                    .set("hostname", "cs1")
-                    .open();
-            System.out.println("Hello!!!");
+            Configuration conf = new PropertiesConfiguration("/opt/titan-1.0.0-hadoop1/conf/audrey.properties");
+            TitanGraph graph = TitanFactory.open(conf);
+            GraphTraversalSource g = graph.traversal();
+            GraphTraversal<Vertex, Vertex> gt = g.V();
+            ArrayList<Vertex> vertices = new ArrayList<Vertex>();
+            while (gt.hasNext()) {
+                vertices.add(gt.next());
+            }
+            for (Vertex vertex : vertices) {
+                System.out.println(vertex.property("name"));
+            }
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
