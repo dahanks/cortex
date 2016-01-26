d3caf159aede8678a029dff23f0cb591ed6a407b
David Horres <david.c.horres@jpl.nasa.gov>
2016-01-25 17:30:54 -0800

    Connect to Apollo and do some graph operation

--- neuron/src/main/java/gov/nasa/jpl/audrey/cortex/neuron/Neuron.java
+++ neuron/src/main/java/gov/nasa/jpl/audrey/cortex/neuron/Neuron.java
@@ -1,37 +1,61 @@
 package gov.nasa.jpl.audrey.cortex.neuron;
 
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 
+import org.apache.activemq.transport.stomp.Stomp.Headers.Subscribe;
+import org.apache.activemq.transport.stomp.StompConnection;
+import org.apache.activemq.transport.stomp.StompFrame;
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
+
 import com.thinkaurelius.titan.core.TitanGraph;
 import com.thinkaurelius.titan.core.TitanFactory;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class Neuron {
 
-    public static void main(String[] args) {
-        try {
-            Configuration conf = new PropertiesConfiguration("/opt/titan-1.0.0-hadoop1/conf/audrey.properties");
-            TitanGraph graph = TitanFactory.open(conf);
-            GraphTraversalSource g = graph.traversal();
-            GraphTraversal<Vertex, Vertex> gt = g.V();
-            ArrayList<Vertex> vertices = new ArrayList<Vertex>();
-            while (gt.hasNext()) {
-                vertices.add(gt.next());
-            }
-            for (Vertex vertex : vertices) {
-                System.out.println(vertex.property("name"));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        } finally {
-            System.exit(0);
-        }
-    }
+    public static void main(String[] args) throws Exception {
+        //set up graph connection
+        Configuration conf = new PropertiesConfiguration("/opt/titan-1.0.0-hadoop1/conf/audrey.properties");
+        TitanGraph graph = TitanFactory.open(conf);
+        GraphTraversalSource g = graph.traversal();
 
+        //set up STOMP connection
+        StompConnection connection = new StompConnection();
+        connection.open("ccd-apollo", 61613);
+        connection.connect("admin", "password");
+        connection.subscribe("/topic/gremlin", Subscribe.AckModeValues.CLIENT);
+        JSONParser parser = new JSONParser();
+
+        while (true) {
+            try {
+                StompFrame message = connection.receive();
+                JSONObject json = (JSONObject) parser.parse(message.getBody());
+                if (json.containsKey("operation")) {
+                    if (json.get("operation").equals("getVertices")) {
+                        GraphTraversal<Vertex, Vertex> gt = g.V();
+                        ArrayList<Vertex> vertices = new ArrayList<Vertex>();
+                        while (gt.hasNext()) {
+                            vertices.add(gt.next());
+                        }
+                        for (Vertex vertex : vertices) {
+                            System.out.println(vertex.property("name"));
+                        }
+                    } else {
+                        System.out.println("Message has no 'operation' field!");
+                    }
+                }
+                connection.ack(message);
+            } catch (SocketTimeoutException e) {
+                System.out.println("Timeout...");
+            }
+        }
+        //connection.disconnect();
+    }
 }
