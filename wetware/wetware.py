1647138befbcfa39169ad749e525974320f80ec6
David Horres <david.c.horres@jpl.nasa.gov>
2016-01-28 17:59:09 -0800

    Parse blueprints and gremlin statements, then send the composed operation to neuron

--- wetware/wetware.py
+++ wetware/wetware.py
@@ -2,11 +2,6 @@
 
 import logging
 import json
-import os
-import glob
-import struct
-import shutil
-from datetime import datetime
 
 from ccd.worker import Worker
 from ccd.worker import WorkerException
@@ -22,6 +17,48 @@ class WetwareWorker(Worker):
         super(WetwareWorker, self).on_message(frame)
         message = json.loads(frame.body)
         ############## End header ##############
+
+        #TODO: do you want to let them mix statements later?
+        if frame.headers['destination'] == self.args['blueprints_dest']:
+            self.convert_to_blueprints(message)
+        elif frame.headers['destination'] == self.args['gremlin_dest']:
+            self.convert_to_gremlin(message)
+        else:
+            raise WetwareException("Statement came on unhandled topic")
+
+    def compose_neuron_operation(self, input_message):
+        output_data = { 'statements': []}
+        for statement in input_message['statements']:
+            output_statement = {'fxns': []}
+            for input_function in statement.split('.'):
+                if input_function == 'graph' or input_function == 'g':
+                    continue
+                else:
+                    function_name = input_function.split('(')[0]
+                    arguments = []
+                    for argument in input_function.split('(')[1].split(')')[0].split(','):
+                        #don't put in empty strings
+                        if argument:
+                            arguments.append(argument[1:-1])
+                    output_function = {
+                        'fxn': function_name,
+                        'args': arguments
+                    }
+                    output_statement['fxns'].append(output_function)
+            output_data['statements'].append(output_statement)
+        return output_data
+
+    def convert_to_blueprints(self, message):
+        output_data = self.compose_neuron_operation(message)
+        output_data['api'] = 'blueprints'
+        print output_data
+        self.publish(output_data)
+
+    def convert_to_gremlin(self, message):
+        output_data = self.compose_neuron_operation(message)
+        output_data['api'] = 'gremlin'
+        print output_data
+        self.publish(output_data)
 
     def define_default_args(self):
         ### This header must not be modified ###
@@ -48,9 +85,9 @@ class WetwareWorker(Worker):
         message = json.loads(frame.body)
         ############## End header ##############
 
-        #for key in ('data', 'data_count', 'host'):
-        #    if key not in message:
-        #        raise FrameException("Message has no {0} field".format(key))
+        for key in ['statements']:
+            if key not in message:
+                raise FrameException("Message has no {0} field".format(key))
 
 class WetwareException(WorkerException):
     pass
