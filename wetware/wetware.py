becee867bef5694193cf0c7162b1905176b6a753
David Horres <david.c.horres@jpl.nasa.gov>
2016-01-28 23:59:34 -0800

    Refactor statement composition; but still needs addEdge

--- wetware/wetware.py
+++ wetware/wetware.py
@@ -19,46 +19,64 @@ class WetwareWorker(Worker):
         ############## End header ##############
 
         #TODO: do you want to let them mix statements later?
-        if frame.headers['destination'] == self.args['blueprints_dest']:
-            self.convert_to_blueprints(message)
-        elif frame.headers['destination'] == self.args['gremlin_dest']:
-            self.convert_to_gremlin(message)
+        if frame.headers['destination'] == self.args['neuron_topic']:
+            self.publish_neuron_operation(message)
         else:
-            raise WetwareException("Statement came on unhandled topic")
+            raise WetwareException("Message came on unhandled topic")
 
-    def compose_neuron_operation(self, input_message):
-        output_data = { 'statements': []}
-        for statement in input_message['statements']:
+    def compose_addEdge_statement(self, statement):
+        #format: Vertex.addEdge(label, Vertex, key, value, ... )
+        output_statement = {'fxns': []}
+        fxn = "addEdge"
+        fromGremlin = statement.split('addEdge')[0]
+
+    def compose_standard_statement(self, statement):
             output_statement = {'fxns': []}
             for input_function in statement.split('.'):
+                #these are the Tinker/TitanGraphs or traversal()
+                # their usage is hardcoded at neuron
                 if input_function == 'graph' or input_function == 'g':
                     continue
                 else:
                     function_name = input_function.split('(')[0]
                     arguments = []
                     for argument in input_function.split('(')[1].split(')')[0].split(','):
-                        #don't put in empty strings, and ignore spaces
+                        #don't put in empty strings
                         if argument:
+                            #strip whitespace, and strip quotes (on each side)
+                            # there would be a problem here is quotes weren't there
                             arguments.append(argument.strip()[1:-1])
                     output_function = {
                         'fxn': function_name,
                         'args': arguments
                     }
                     output_statement['fxns'].append(output_function)
-            output_data['statements'].append(output_statement)
-        return output_data
+            return output_statement
 
-    def convert_to_blueprints(self, message):
-        output_data = self.compose_neuron_operation(message)
-        output_data['api'] = 'blueprints'
+    def publish_neuron_operation(self, input_message):
+        output_data = { 'statements': []}
+        for raw_statement in input_message['statements']:
+            if "addVertex" in raw_statement or 'addEdge' in raw_statement:
+                output_data['statements'].append(self.compose_blueprints_statement(raw_statement))
+            else:
+                output_data['statements'].append(self.compose_gremlin_statement(raw_statement))
         logging.debug(output_data)
         self.publish(output_data)
 
-    def convert_to_gremlin(self, message):
-        output_data = self.compose_neuron_operation(message)
-        output_data['api'] = 'gremlin'
-        logging.debug(output_data)
-        self.publish(output_data)
+    def compose_blueprints_statement(self, raw_statement):
+        #addEdge is a special case, addVertex is not (and don't expect others to be)
+        if 'addEdge' in raw_statement:
+            pass
+        else:
+            statement = self.compose_standard_statement(raw_statement)
+        statement['api'] = 'blueprints'
+        return statement
+
+    def compose_gremlin_statement(self, raw_statement):
+        #no special cases for Gremlin
+        statement = self.compose_standard_statement(raw_statement)
+        statement['api'] = 'gremlin'
+        return statement
 
     def define_default_args(self):
         ### This header must not be modified ###
