21b9129ecf35859a89af809b02a0698387147768
David Horres <david.c.horres@jpl.nasa.gov>
2016-02-01 14:37:32 -0800

    Initial working stomp message and response to wetware

--- audrey-chat/js/audrey-chat.js
+++ audrey-chat/js/audrey-chat.js
@@ -1,13 +1,30 @@
 var main = function() {
-    draw_modreq();
+    draw_audrey();
+    draw_you();
+    setup_websocket(location.host);
 }
 
-function draw_modreq() {
-    draw_table();
+function draw_audrey() {
+    $("#audrey").append("<div id=audrey-dialog class=dialog></div>");
+    $("#audrey").append("<div id=audrey-avatar></div>");
+}
+function draw_you() {
+    $("#you").append("<div id=you-dialog class=dialog contenteditable></div>");
+    $("#you-dialog").keydown(function(e) {
+        var key = e.which;
+        if (key == 13) {
+            submit_chat($(this).text());
+        }
+    });
 }
 
-function draw_table() {
-    $("#bulk-modreq").append("<table id=modreq-table></table>");
+function submit_chat(text_input) {
+    $("#you-dialog").empty();
+    submitAudreyChat(text_input);
+}
+
+function handle_audrey_response(message) {
+    $("#audrey-dialog").text(message);
 }
 
 $(document).ready(main);
---
+++ audrey-chat/js/ws.js
@@ -0,0 +1,68 @@
+var client;
+var url = "ws://" + location.host + ":61623";
+var user = "admin";
+var password = "password";
+var wetware_topic = "/topic/wetware.nlp";
+var reply_topics = [];
+
+// apollo_host can be set from the URL. Otherwise it defaults the server it is deployed on
+// http://localhost:8888/ccd-web-git/beam_hlr_tabs.html?apollo_host=137.78.81.99
+function setup_websocket(apollo_host) {
+    if (window.WebSocket) {
+        client = Stomp.client(url);
+        client.connect(user, password, connect_callback, error_callback);
+    } else {
+        console.log("You're browser does not support WebSockets!");
+    }
+}
+
+function on_message(message) {
+    try {
+        var dest = message['headers']['destination'];
+        var body = JSON.parse(message['body']);
+        if (dest.startsWith('/queue/temp')) {
+            handle_audrey_response(message);
+        } else {
+            console.log("Not sure where this message came from...dropping...");
+        }
+    } catch (err) {
+        console.log("Malformed message response");
+    }
+}
+
+function connect_callback(frame) {
+//    audrey_subscription = client.subscribe(audrey_topic, on_message);
+}
+
+function error_callback(error) {
+    var json_data_str = JSON.stringify(error);
+    console.log("Error: " + JSON.stringify(error));
+    //TODO: verify that this works at all
+    if (error['headers']) {
+        var message = error['headers']['message'];
+        console.log("Error: " + message);
+        if (message.indexOf("Stale connection") != -1){
+            establishConnection();
+        }
+    }
+}
+
+function submitAudreyChat(statement) {
+    var json_data = {'statements': [statement]}
+    var json_data_str = JSON.stringify(json_data);
+    var reply_to_topic = '/temp-queue/' + guid();
+    var headers = {'reply-to': reply_to_topic};
+    reply_topics.push(reply_to_topic);
+    client.subscribe(reply_to_topic, on_message);
+    client.send(wetware_topic, headers, json_data_str);
+}
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
