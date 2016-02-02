30ca72d0d3d332273019b004ba1ab688b6138bd7
David Horres <david.c.horres@jpl.nasa.gov>
2016-02-01 16:03:21 -0800

    Add various reactions

--- audrey-chat/js/audrey-chat.js
+++ audrey-chat/js/audrey-chat.js
@@ -7,6 +7,7 @@ var main = function() {
 function draw_audrey() {
     $("#audrey").append("<div id=audrey-dialog class=dialog></div>");
     $("#audrey").append("<div id=audrey-avatar></div>");
+    $("#audrey-avatar").append("<img id=audrey-img src='img/idle.gif'>");
 }
 function draw_you() {
     $("#you").append("<div id=you-dialog class=dialog contenteditable></div>");
@@ -20,11 +21,30 @@ function draw_you() {
 
 function submit_chat(text_input) {
     $("#you-dialog").empty();
+    $("#audrey-dialog").empty();
     submitAudreyChat(text_input);
 }
 
 function handle_audrey_response(message) {
+    $("#audrey-dialog").empty();
     $("#audrey-dialog").text(message);
+    var img = document.getElementById("audrey-img");
+    if (message.indexOf("Yes,") > -1) {
+        img.src = 'img/yes.gif';
+        setTimeout(function() {
+            img.src = 'img/idle.gif';
+        }, 2650);
+    } else if (message.indexOf("No,") > -1) {
+        img.src = 'img/no.gif';
+        setTimeout(function() {
+            img.src = 'img/idle.gif';
+        }, 2800);
+    } else {
+        img.src = 'img/neutral.gif';
+        setTimeout(function() {
+            img.src = 'img/idle.gif';
+        }, 2400);
+    }
 }
 
 $(document).ready(main);
