8fb8006ef39975b4c00e690bd7e01132eb756b52
David Horres <david.c.horres@jpl.nasa.gov>
2016-02-26 11:11:54 -0800

    Connecting, subscribing, printing messages

--- wetware-py/modules/ngfr-sensors/ngfr-sensors.py
+++ wetware-py/modules/ngfr-sensors/ngfr-sensors.py
@@ -24,10 +24,11 @@ class WetwareWorker(Worker):
             self.mqtt_run()
 
     def mqtt_on_connect(self, client, userdata, flags, rc):
-        pass
+        self.mqtt_client.subscribe("global/#")
 
-    def mqtt_on_message(self, client, userdata, msg):
-        pass
+    def mqtt_on_message(self, client, userdata, message):
+        logging.debug("Received MQTT message on topic: {0}".format(message.topic))
+        logging.debug("Message: {0}".format(message.payload))
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
