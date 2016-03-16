8682d91351f3f2140c9c2b4bd20ddd5e9577c332
David Horres <david.c.horres@jpl.nasa.gov>
2016-03-16 14:22:11 -0700

    Clean up test case a bit

--- wetware-py/modules/greenado/greenado.py
+++ wetware-py/modules/greenado/greenado.py
@@ -24,39 +24,32 @@ class WetwareWorker(Worker):
         message = json.loads(frame.body)
         ############## End header ##############
 
-        #transaction will be none if we don't need to reply
         if frame.headers['destination'] == self.args['input_topic']:
-            # secs = self.publish(message, expect_reply=True)
-            # self.after_secs(secs)
-
             if float(message['secs']) == 1:
-                #time.sleep(1)
-                #self.reply({'seconds': 'that was JUST one second'})
-                self.publish(message, callback=self.after_one, transaction=transaction)
+                # TEST SYNCRONOUS
+                time.sleep(1)
+                self.reply({'seconds': 'that was JUST one second'})
+                # TEST ASYNCRONOUS
+                #self.publish(message, callback=self.after_one, transaction=transaction)
             elif float(message['secs']) == 5:
-                #time.sleep(5)
-                #self.reply({'seconds': 'that was TOTALLY five seconds'})
-                self.publish(message, callback=self.after_five, transaction=transaction)
+                # TEST SYNCRONOUS
+                time.sleep(5)
+                self.reply({'seconds': 'that was TOTALLY five seconds'})
+                # TEST ASYNCRONOUS
+                #self.publish(message, callback=self.after_five, transaction=transaction)
             else:
                 logging.debug(message)
 
-    # def handle_reply(self, frame, transaction):
-    #     super(WetwareWorker, self).handle_reply(frame, transaction)
-    #     message = json.loads(frame.body)
-    #     print frame.headers['destination']
-    #     print message
-
-    def run(self):
-        with ApolloConnection(self.args) as self.apollo_conn:
-            self.publish({'secs': '1'}, callback=self.after_one)
-            while True:
-                frame = self.apollo_conn.receiveFrame()
-                logging.info("Received message: {0}".format(frame.info()))
-                self.on_message(frame)
-                break;
-
-    def after_secs(self, secs):
-        self.publish({'seconds':'that was {0} seconds'.format(secs)})
+    # UNCOMMENT THIS TO RUN ASYNC and SYNC TESTS
+    # Test for simply sending a message and receiving a reply
+    # def run(self):
+    #     with ApolloConnection(self.args) as self.apollo_conn:
+    #         self.publish({'secs': '1'}, callback=self.after_one)
+    #         while True:
+    #             frame = self.apollo_conn.receiveFrame()
+    #             logging.info("Received message: {0}".format(frame.info()))
+    #             self.on_message(frame)
+    #             break;
 
     def after_one(self, frame, destination):
         #self.reply({'seconds':'that was one second'})
@@ -65,6 +58,12 @@ class WetwareWorker(Worker):
     def after_five(self, frame, destination):
         #self.reply({'seconds':'that was five second'})
         self.publish({'seconds':'that was five seconds'}, destination)
+
+    def handle_reply(self, frame, transaction):
+        super(WetwareWorker, self).handle_reply(frame, transaction)
+        message = json.loads(frame.body)
+        print "GOT BACK THIS MESSAGE:"
+        print message
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
