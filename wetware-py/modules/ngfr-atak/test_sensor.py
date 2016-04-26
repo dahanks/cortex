e0689ac22a3c5416b36deae9a9308d1a42b44ff1
David Horres <david.c.horres@jpl.nasa.gov>
2016-04-26 16:38:35 -0700

    Basic sensor that waits for event registration, then sends one even out

---
+++ wetware-py/modules/ngfr-atak/test_sensor.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import logging
+import sys
+import json
+import time
+
+from wetware.worker import Worker
+
+class WetwareWorker(Worker):
+
+    def on_message(self, frame):
+        ### This header must not be modified ###
+        transaction = super(WetwareWorker, self).on_message(frame)
+        message = json.loads(frame.body)
+        ############## End header ##############
+        print json.dumps(message, sort_keys=True, indent=4, separators=(',', ': '))
+        if 'topic' in message:
+            time.sleep(10)
+            event_data = {'event': 'some event happened!'}
+            print event_data
+            self.publish(event_data, topic=message['topic'])
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    listener = WetwareWorker("wetware")
+    listener.run()
+
+if __name__ == "__main__":
+    main()
