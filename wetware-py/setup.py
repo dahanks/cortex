ffe1090ef4df36d79eda224a3c82f5e00e149b54
David Horres <david.c.horres@jpl.nasa.gov>
2016-05-06 13:06:27 -0700

    Bump to 1.1.3

--- wetware-py/setup.py
+++ wetware-py/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='wetware-py',
-      version='1.1.2',
+      version='1.1.3',
       packages=['wetware'],
       install_requires=['stompest>=2.1.6'],
 )
