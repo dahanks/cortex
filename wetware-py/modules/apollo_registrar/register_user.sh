29092e4e72f1bc7d2a37644a065397c4b6926f17
David Horres <david.c.horres@jpl.nasa.gov>
2016-10-26 15:47:44 -0700

    Don't add users a second time

--- wetware-py/modules/apollo_registrar/register_user.sh
+++ wetware-py/modules/apollo_registrar/register_user.sh
@@ -6,15 +6,19 @@ BASE='/opt/apollo-broker'
 #Encrypt password
 pass=`$EXEC $BASE/bin/apollo-broker encrypt $2`
 
-#Delete existing/add user
-$EXEC sed -i /^$1=/d $BASE/etc/users.properties
-$EXEC bash -c "echo \"$1=$pass\" >> $BASE/etc/users.properties"
-
-#Grab existing users
-users=`$EXEC grep ^users= $BASE/etc/groups.properties`
+if [[ -z `$EXEC grep ^$1= $BASE/etc/users.properties` ]]; then
+    #Delete existing/add user
+    $EXEC sed -i /^$1=/d $BASE/etc/users.properties
+    $EXEC bash -c "echo \"$1=$pass\" >> $BASE/etc/users.properties"
+else
+    exit 1
+fi
 
 #Add new user to group if not already there
 if [[ -z `$EXEC grep "[=|]$1|" $BASE/etc/groups.properties` ]]; then
+    #Grab existing users
+    users=`$EXEC grep ^users= $BASE/etc/groups.properties`
+
     $EXEC sed -i /users/d $BASE/etc/groups.properties && \
     $EXEC bash -c "echo \"$users$1|\" >> $BASE/etc/groups.properties"
 fi
