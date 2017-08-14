b5739a0b2f32e369687b6109c41a4af7dd8a0c78
Paloulian <gpalouli@ndc.nasa.gov>
2017-08-14 11:36:39 -0700

    updated the ake-bridge to utilize new code from Tony:20170811. Better use of link properties.

--- wetware-py/modules/ake-bridge/ake_bridge.py
+++ wetware-py/modules/ake-bridge/ake_bridge.py
@@ -20,13 +20,17 @@ class WetwareWorker(Worker):
         if frame.headers['destination'] == self.args['input_topic']:
             try:
                 self.process_ake_json(message, transaction)
-            except KeyError:
+            except KeyError, e:
                 logging.error("Received invalid TSPI JSON structure")
+                logging.error(e)
+                raise
 
     def process_ake_json(self, json_data, transaction):
         if json_data['Ontology']['Name'] != "TSPI":
             logging.warning("Received ontology other than TSPI. Ignoring")
             return
+
+        logging.debug(json_data)
         
         statements = Statements()
         for node in json_data['Nodes']:
@@ -39,26 +43,13 @@ class WetwareWorker(Worker):
                         statements.add_vertex_property(name,prop,node[name]['rules'][prop])
         for edge in json_data['Links']:
             for name in edge:
-                print edge
-                if edge[name]['rules']==None:
-                    statements.add_edge(edge[name]['source'],'next',edge[name]['target'])
-                else:
-                    statements.add_edge(edge[name]['source'],"next%s"%edge[name]['rules']['Conditional'],edge[name]['target'])
-        self.publish(statements, callback=self.print_results)
-
-    def run_setup(self):
-        pass
-        #self.query_for_ake()
-
-    def query_for_ake(self):
-        statements = Statements()
-        statements.gremlin("g.V().valueMap()")
-        self.publish(statements, callback=self.print_results)
+                statements.add_edge(edge[name]['source'],'next',edge[name]['target'],edge[name]['rules'])
+        print "publishing the script"
+        self.publish(statements, callback=self.result_callback)
 
     def print_results(self, frame, context, transaction):
         responses = Responses(frame)
-        vertices = responses.get_vertex_objects()
-        logging.info(vertices)
+        logging.debug(responses)
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
