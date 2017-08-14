afcc76b78af0254361c8dfed596d05971dc1389b
Paloulian <gpalouli@ndc.nasa.gov>
2017-08-14 14:23:19 -0700

    added Tony's update to reasoner to retrieve and process graph links with properties

--- wetware-py/modules/ake-reasoner/ake_reasoner.py
+++ wetware-py/modules/ake-reasoner/ake_reasoner.py
@@ -25,50 +25,33 @@ class WetwareWorker(Worker):
                 self.performScript(message['procedure'])
             except KeyError:
                 logging.exception("Something went wrong")
-
-    #def performScript(self,name):
-    #    ''' Get the named node's execution data (if any) and the names of its child nodes '''
-    #    statements = Statements()
-    #    statements.get_vertex_property(name,'Execute')
-    #    statements.get_vertex_property(name,'Param')
-    #    statements.gremlin('g.V().has("name","'+name+'").out("child").values("name")')
-    #    self.publish(statements, callback=self.perform_callback)
     
     def performScript(self,name):
         ''' Get the named node's execution data (if any) and the names of its child nodes '''
         statements = Statements()
         statements.gremlin('g.V().has("name","'+name+'").valueMap()')
+        print "starting script execution"
         self.publish(statements, callback=self.perform_callback)
 
-    #def perform_callback(self, frame, context, transaction):
-    #    ''' Take the returned value of the performScript query, call the python script (if any), and then move on to querying the children. '''
-    #    responses = Responses(frame)
-    #    logging.debug(responses)
-    #    logging.debug(responses[0])
-    #    logging.debug(responses[1])
-    #    if responses[0]!=u'':
-    #        call(['python', responses[0]]+responses[1].split())
-    #    if responses[2]!=u'[]':
-    #        for r in responses[2][1:-1].split(", "):
-    #            self.performScript(r)
-    
     def perform_callback(self, frame, context, transaction):
         ''' Take the returned value of the performScript query, call the python script (if any), and then move on to querying the children. '''
         responses = Responses(frame)
         print responses
         nodes = responses.get_vertex_objects()
         for n in nodes:
-            print n
-            label = 'next'
+            # print n
+            statements = Statements()
+            output = ''
             if 'Execute' in n:
                 p = (sub.Popen(['python', n['Execute']],stdout=sub.PIPE,stderr=sub.PIPE) if 'Param' not in n else
                      sub.Popen(['python', n['Execute'], n['Param']],stdout=sub.PIPE,stderr=sub.PIPE))
                 output, errors = p.communicate()
                 if len(output)>0: output = output[:-1] # strip off the thrailing CR
-                label = label+output
-                print ">>%s<<"%output
-            statements = Statements()
-            statements.gremlin('g.V().has("name","'+n['name']+'").out("'+label+'").valueMap()')
+                #print ">>%s<<"%output
+            if output == '':
+                statements.gremlin('g.V().has("name","'+n['name']+'").out("next").valueMap()')
+            else:
+                statements.gremlin('g.V().has("name","'+n['name']+'").outE("next").has("Conditional","'+output+'").inV().valueMap()')
             self.publish(statements, callback=self.perform_callback)
 
 def main():
