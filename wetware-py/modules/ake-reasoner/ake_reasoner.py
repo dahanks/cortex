830f8ec2b1077f24b30844647ed84e18ecd586fe
Paloulian <gpalouli@ndc.nasa.gov>
2017-08-28 15:47:18 -0700

    working reasoner with param input and ability to save based on seen Parameter node type.

--- wetware-py/modules/ake-reasoner/ake_reasoner.py
+++ wetware-py/modules/ake-reasoner/ake_reasoner.py
@@ -13,6 +13,9 @@ class WetwareWorker(Worker):
     def __init__(self, subclass_section):
         super(WetwareWorker, self).__init__(subclass_section)
 
+        self.parameters = {}
+        self.lastParam = ''
+
     def on_message(self, frame):
         ### This header must not be modified ###
         transaction = super(WetwareWorker, self).on_message(frame)
@@ -41,18 +44,47 @@ class WetwareWorker(Worker):
         for n in nodes:
             print n
             statements = Statements()
+            returnValue = ''
             output = ''
+            param = ''
             if 'Execute' in n:
-                p = (sub.Popen(['python', n['Execute']],stdout=sub.PIPE,stderr=sub.PIPE) if 'Param' not in n else
-                     sub.Popen(['python', n['Execute'], n['Param']],stdout=sub.PIPE,stderr=sub.PIPE))
-                output, errors = p.communicate()
-                #if len(output)>0: output = output[:-1] # strip off the thrailing CR
-                if len(output)>0: output = output.strip()
-                print ">>%s<<"%output
+                parameter = ''
+                if 'Param' in n and n['Param'] in self.parameters:
+                    parameter = self.parameters[n['Param']]
+                    print "Parameter [%s] found: [%s]" % (n['Param'], parameter)
+                
+                p = (sub.Popen(['python', n['Execute']], stdout=sub.PIPE, stderr=sub.PIPE) if 'Param' not in n else
+                     sub.Popen(['python', n['Execute'], parameter], stdout=sub.PIPE, stderr=sub.PIPE))
+                returnValue, errors = p.communicate()
+
+                if len(returnValue)>0: returnValue = returnValue.strip()
+                print "Return Value: >>%s<<"%returnValue
+
+                # split the return value here
+                splitReturnValue = returnValue.split(',')
+                if len(splitReturnValue) == 2:
+                    output = splitReturnValue[0]
+                    param = splitReturnValue[1]
+                    if len(output)>0: output = output.strip()
+                    if len(param)>0: param = param.strip()
+                    self.lastParam = param
+                else:
+                    if len(splitReturnValue) == 1:
+                        output = splitReturnValue[0]
+                        if len(output)>0: output = output.strip()
+
+                print "Output: >>%s<<"%output
+                print "Last Known Param: >>%s<<"%self.lastParam
+
+            if 'Param' in n and n['type'] == 'Parameter':
+                self.parameters[n['Param']] = self.lastParam
+                print "Set Parameter [%s] to [%s]" % (n['Param'], self.lastParam)
+                
             if 'Execute' in n and n['type'] == 'Execute_Branch':
                 statements.gremlin('g.V().has("name","'+n['name']+'").outE("next").has("Conditional","'+output+'").inV().valueMap()')
             else:
                 statements.gremlin('g.V().has("name","'+n['name']+'").out("next").valueMap()')
+                
             self.publish(statements, callback=self.perform_callback)
 
 def main():
